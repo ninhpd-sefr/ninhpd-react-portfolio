cbeea5d07c12be0013f967da8d3abb425d7c80cc
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2023-02-10 11:18:02 +0700

    complete the first

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion'
 
 import { images } from '../../constants';
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import './About.scss'
 import { urlFor,client } from '../../client';
 
@@ -50,4 +50,9 @@ const About = () => {
     );
 };
 
-export default AppWrap(About,'about');
+
+export default AppWrap(
+    MotionWrap(About, 'app__about'),
+    'about',
+    'app__whitebg',
+  );
