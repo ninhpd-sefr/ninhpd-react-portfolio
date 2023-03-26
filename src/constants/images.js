b1b09fec3fd86c8caf80c4a8e22738f6358a14d7
phamdangninh171102 <89170990+phamdangninh171102@users.noreply.github.com>
2023-03-26 22:10:44 +0700

    change slider

--- src/constants/images.js
+++ src/constants/images.js
@@ -26,6 +26,8 @@ import about04 from '../assets/about04.png';
 import profile from '../assets/profile.png';
 import profile1 from '../assets/profile1.png';
 import profile2 from '../assets/profile2.png';
+import profile3 from '../assets/profile3.png';
+
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 import dev from '../assets/dev.png';
@@ -65,6 +67,7 @@ export default {
   profile,
   profile1,
   profile2,
+  profile3,
   circle,
   logo,
   dev,
